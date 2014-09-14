c9ebf8478bbf882354c17702da604a0b83b7c529
JUN <lijunzhu123@gmail.com>
2014-09-14 21:54:59 +1000

    final

--- matrix.c
+++ matrix.c
@@ -28,13 +28,10 @@ void initMatrix(Entity ** matrix, int size_x, int size_y)
     srand48(8767134);
 
     int i, j;
-    int choose = rand()%10;
 
     for (i = 1; i <= size_y; i++)
     {
         for (j = 1; j <= size_x; j++)
-<<<<<<< HEAD
-<<<<<<< HEAD
         {
             if(drand48() <= 0.10){
                 matrix[i][j].type = ZOMBIE;
@@ -43,25 +40,7 @@ void initMatrix(Entity ** matrix, int size_x, int size_y)
                 matrix[i][j].type = HUMAN;
                 matrix[i][j].moveChance = MOVE_HUMAN;
             }
-=======
-=======
->>>>>>> FETCH_HEAD
-        {  if (choose >2)
-           {
-            imatrix[i][j].type = HUMAN;
-            matrix[i][j].moveChance = MOVE_HUMAN;
-           }
-           else 
-           {
-            imatrix[i][j].type = ZOMBIE;
-            matrix[i][j].moveChance = MOVE_ZOMBIE;
-            }
-          
-<<<<<<< HEAD
->>>>>>> FETCH_HEAD
-=======
->>>>>>> FETCH_HEAD
-            
+
         }
     }
 }
