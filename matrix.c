0d9f37a920e3055b697ca95006ffa955cd6b46d6
hxd33602 <hxd33602@gmail.com>
2014-09-14 19:59:28 +1000

    INIT RAND EMPTY, HUMAN and ZOMBIE
    初始化需要并行运算吗

--- matrix.c
+++ matrix.c
@@ -26,13 +26,24 @@ Entity **createMatrix(int size_x, int size_y)
 void initMatrix(Entity ** matrix, int size_x, int size_y)
 {
     int i, j;
+    int choose = rand()%10;
 
     for (i = 1; i <= size_y; i++)
     {
         for (j = 1; j <= size_x; j++)
-        {
-            matrix[i][j].type = HUMAN;
+        {  if (choose > 5)
+           {
+            imatrix[i][j].type = HUMAN;
             matrix[i][j].moveChance = MOVE_HUMAN;
+           }
+           else if (choose <2)
+           {
+            imatrix[i][j].type = ZOMBIE;
+            matrix[i][j].moveChance = MOVE_ZOMBIE;
+            }
+            else
+                {imatrix[i][j].type = EMPTY;}
+            
         }
     }
 }
