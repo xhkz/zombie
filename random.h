ce5707c5b3e839d457e35aac4f6e4e00bd86c273
JUN <lijunzhu123@gmail.com>
2014-09-25 13:30:20 +1000

    add random.h

--- random.h
+++ random.h
@@ -1,10 +1,6 @@
 #ifndef RANDOM_H_INCLUDED
 #define RANDOM_H_INCLUDED
 
-static void init_twister(void);
-static void sgenrand(unsigned long int rng_num, unsigned long seed);
-static unsigned long genrand(unsigned long int rng_num);
-void initRandom();
 double drandom(void);
 int irandom(int limit);
 
